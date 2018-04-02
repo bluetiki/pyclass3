63b26b068fc5155636839b7fd53f39b94ef51171
bluetiki <bluetiki@users.noreply.github.com>
2018-04-01 21:43:32 -0700

    updating

--- telnet.py
+++ telnet.py
@@ -3,36 +3,40 @@
 import telnetlib
 import time
 import getpass
+import socket
+import sys
 
+def telnet_connect(ip,port=23,timeout=10):
+    try:
+        telnet_conn = telnetlib.Telnet(ip,port,timeout)
+    except socket.timeout:
+        sys.exit('connection timed out')
+    return telnet_conn
 
-TELNET_PORT = 23
-TELNET_TIMEOUT = 10
-
-def connect(ip,user,pw):
-    remote_conn = telnetlib.Telnet(ip,TELNET_PORT,TELNET_TIMEOUT)
-    remote_conn.read_until(b"sername:",TELNET_TIMEOUT).decode('utf-8', 'ignore')
-    remote_conn.write((user + '\n').encode('utf-8'))
-    remote_conn.read_until(b"assword:",TELNET_TIMEOUT).decode('utf-8', 'ignore')
-    remote_conn.write((pw + '\n').encode('utf-8'))
-    time.sleep(1)
-    remote_conn.read_very_eager().decode('utf-8')
-    return remote_conn
+def login(telnet_conn,tries=3,timeout=10):
+    user = input('Please enter your username: ')
+    pw = getpass.getpass()
+    output = telnet_conn.read_until(b"sername:",timeout).decode('utf-8', 'ignore')
+    telnet_conn.write((user + '\n').encode('utf-8'))
+    output += telnet_conn.read_until(b"assword:",timeout).decode('utf-8', 'ignore')
+    telnet_conn.write((pw + '\n').encode('utf-8'))
+    return output
 
 def send_cmd(cmd,conn):
     conn.write((cmd + '\n').encode('utf-8'))
     time.sleep(1)
-    output = conn.read_very_eager().decode('utf-8')
-    return output
+    out = conn.read_very_eager().decode('utf-8').splitlines(keepends=True)
+    del out[0]
+    del out[-1]
+    return "".join(out)
 
 
 def main():
 
-    ip = input('Please input target ip address: ')
-    user = input('Please enter your username: ')
-    pw = getpass.getpass()
+    ip = input('Please input target IP Address: ')
 
-    conn = connect(ip,user,pw)
-    
+    conn = telnet_connect(ip)
+    login(conn)
     send_cmd('ter len 0',conn)
     out  = send_cmd('show ver',conn)
     print (out)
